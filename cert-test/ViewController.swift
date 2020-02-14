94b73a84e0f42c7ee89782587b5e802480152cbb
Oscar Fredriksson <osfr1701@student.miun.se>
2020-02-14 11:45:27 +0100

    Initial Commit

---
+++ cert-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cert-test
+//
+//  Created by Oscar Fredriksson on 2020-02-14.
+//  Copyright © 2020 Oscar Fredriksson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
